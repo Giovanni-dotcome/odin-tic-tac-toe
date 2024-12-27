1a9585008fdb9d40e7cc07f43eb65b723f10a200
giochac <giodevito99@gmail.com>
2024-12-27 12:07:29 +0100

    refactored dom elements inside an elements object

--- src/createDOM.js
+++ src/createDOM.js
@@ -1,13 +1,17 @@
-export function createDOM(game, turn, grid, footer) {
+export function createDOM(game, elements) {
+  elements.reset.addEventListener('click', () => {
+    game.reset()
+    display()
+  });
   const createBoard = () => {
     const board = game.getBoard;
-    grid.innerHTML = ``;
+    elements.grid.innerHTML = ``;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        grid.append(createCell(i, j, board[i][j]));
+        elements.grid.append(createCell(i, j, board[i][j]));
       }
     }
-    return grid;
+    return elements.grid;
   }
 
   const createCell = (y, x, marker) => {
@@ -25,10 +29,10 @@ export function createDOM(game, turn, grid, footer) {
 
   const display = () => {
     // TODO: inefficient code, create a way in which you do query selector once, and rerender only the cell is clicked (without rerendering all the board)
-    turn.innerText = game.getPlayer1Turn() ? 'X' : 'O';
-    footer.querySelector('#scoreY').innerText = game.getScore().yWins;
-    footer.querySelector('#scoreX').innerText = game.getScore().xWins;
-    footer.querySelector('#scoreTies').innerText = game.getScore().ties;
+    elements.turn.innerText = game.getPlayer1Turn() ? 'X' : 'O';
+    elements.scoreY.innerText = game.getScore().yWins;
+    elements.scoreX.innerText = game.getScore().xWins;
+    elements.scoreTies.innerText = game.getScore().ties;
     createBoard();
   };
 
--- src/main.js
+++ src/main.js
@@ -4,20 +4,19 @@ import { createGame } from './createGame.js';
 
 const markerX = "\u2717";
 const markerO = "\u2B58";
+const elements = {
+  turn: document.querySelector('#turn'),
+  grid: document.querySelector('#grid'),
+  scoreX: document.querySelector('#scoreX'),
+  scoreY: document.querySelector('#scoreY'),
+  scoreTies: document.querySelector('#scoreTies'),
+  reset: document.querySelector('#reset')
+}
 
-// Example usage
-const turn = document.querySelector('#turn');
-const grid = document.querySelector('#grid');
-const footer = document.querySelector('footer');
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', () => {
-  game.reset()
-  DOM.display()
-});
+
 const gio = createPlayer('Gio', markerX);
 const vale = createPlayer('Vale', markerO);
 const game = createGame(gio, vale);
-const DOM = createDOM(game, turn, grid, footer);
+const DOM = createDOM(game, elements);
 
 DOM.display();
-//
